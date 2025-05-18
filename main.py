2564deb0a511b9e60645741d3c92cc22776f3f1c
Jaziel Aarón Carballo Tadeo <jaziel.carballo@gmail.com>
2025-05-18 00:31:46 -0600

    Update main.py
    El id se agrega para que el json lo devuelva también

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
+# Regex para extraer nombre, ELO, ranking y winrate
 pattern = r"^.{1,2}\s{2}(.+?)\s\((\d+)\)\sRank\s#(\w+),.*?(\w+)%\swinrate"
 
 @app.get("/estadisticas")
@@ -22,11 +23,13 @@ def obtener_estadisticas(id: list[int] = Query(...)):
         url = f"https://data.aoe2companion.com/api/nightbot/rank?profile_id={pid}"
         response = requests.get(url)
         texto = response.text.strip().replace("\xa0", " ").strip('"')
+
         match = re.search(pattern, texto)
         if match:
             ranking = match.group(3)
             winrate = match.group(4)
             data.append({
+                "id": pid,  # 👈 aquí se agrega el ID manualmente al resultado
                 "nombre": match.group(1),
                 "elo": int(match.group(2)),
                 "ranking": int(ranking) if ranking.isdigit() else None,
